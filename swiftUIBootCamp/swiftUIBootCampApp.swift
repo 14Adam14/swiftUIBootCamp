7aceb9a585c8d8d1730c76e30056744cac003f73
Adham <adam.bolotov@gmail.com>
2022-02-18 00:53:33 +0100

    Initial Commit

---
+++ swiftUIBootCamp/swiftUIBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIBootCampApp.swift
+//  swiftUIBootCamp
+//
+//  Created by user213083 on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
