d9df804e5d15d291ae6d93900dabf839ea6cd9bc
Adham <adam.bolotov@gmail.com>
2022-02-18 02:05:42 +0100

    finish Text lesson

--- swiftUIBootCamp/TextBootCamp.swift
+++ swiftUIBootCamp/TextBootCamp.swift
@@ -9,15 +9,27 @@ import SwiftUI
 
 struct TextBootCamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .font(.body)
-        //  .fontWeight(.semibold)
-            .bold()
-        //  .underline()
-            .italic()
-        //  .strikethrough()
-            .underline(true, color: Color.red)
-            .strikethrough(true, color: Color.brown)
+        Text("Hello, World! Mery ilpe aery nery pe. Netip n45 bery ldiazery Hello, World! Mery ilpe aery nery pe. Netip n45 bery ldiazery".capitalized)
+        
+//            .font(.body)
+//        //  .fontWeight(.semibold)
+//            .bold()
+//        //  .underline()
+//            .italic()
+//        //  .strikethrough()
+//            .underline(true, color: Color.red)
+//            .strikethrough(true, color: Color.brown)
+        
+        
+            
+         //   .font(.system(size: 24, weight: .semibold, design: .serif))
+        
+        //    .baselineOffset(10.0)
+        //    .kerning(1.0)
+              .multilineTextAlignment(.leading)
+              .foregroundColor(.red)
+              .frame(width: 200, height: 100, alignment: .leading)
+              .minimumScaleFactor(0.1)
     }
 }
 
