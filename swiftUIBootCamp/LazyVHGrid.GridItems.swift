a5a68c130fb7042faf56b39b0d5bf98bd0c6cd0e
Adham <adam.bolotov@gmail.com>
2022-02-20 09:40:15 +0100

    in video  have more,    etc pinned headers

---
+++ swiftUIBootCamp/LazyVHGrid.GridItems.swift
@@ -0,0 +1,45 @@
+
+
+import SwiftUI
+
+
+
+
+struct LazyVHGrid_GridItems: View {
+    
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil),
+        GridItem(.flexible(), spacing: 6, alignment: nil)
+
+    ]
+    
+    
+    var body: some View {
+        
+        ScrollView {
+            
+            Rectangle()
+                .fill(Color.white)
+                .frame(height: 400)
+            
+            LazyVGrid(columns: columns) {
+                ForEach(0..<50) { index in
+                    Rectangle()
+                        .frame(height: 150)
+                }
+            }
+        }
+      
+        
+        
+        
+    }
+}
+
+struct LazyVHGrid_GridItems_Previews: PreviewProvider {
+    static var previews: some View {
+        LazyVHGrid_GridItems()
+    }
+}
