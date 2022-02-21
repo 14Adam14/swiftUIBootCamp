ffa2e3a98d7e62a36da074840c534f4e815bd7e9
Adham <adam.bolotov@gmail.com>
2022-02-21 06:48:17 +0100

    end   extract func subview

---
+++ swiftUIBootCamp/ExtractFuncSubv.swift
@@ -0,0 +1,65 @@
+
+import SwiftUI
+
+struct ExtractFuncSubv: View {
+    
+    
+    @State var backgroundColor: Color = Color.pink
+    
+    
+    var body: some View {
+       
+        
+        
+        
+        ZStack {
+            //backgr
+            backgroundColor
+                .edgesIgnoringSafeArea(.all)
+            
+ 
+           //content
+            contentLayer
+
+        }
+    }
+   
+    
+    
+    var contentLayer: some View {
+        VStack {
+            Text("title")
+                .font(.largeTitle)
+            
+            Button {
+                buttPess()
+            } label: {
+                Text("prasse")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                  
+            }
+
+            
+        }
+    }
+    
+    
+    
+    func buttPess() {
+        backgroundColor = .yellow
+    }
+    
+}
+
+
+
+
+struct ExtractFuncSubv_Previews: PreviewProvider {
+    static var previews: some View {
+        ExtractFuncSubv()
+    }
+}
