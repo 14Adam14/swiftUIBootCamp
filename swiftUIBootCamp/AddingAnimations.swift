6df01a9ce7e67d801cb9966cd99ebe4b48fb3302
Adham <adam.bolotov@gmail.com>
2022-02-22 14:24:12 +0100

    end anima

---
+++ swiftUIBootCamp/AddingAnimations.swift
@@ -0,0 +1,50 @@
+
+import SwiftUI
+
+struct AddingAnimations: View {
+    
+    
+    @State var isAnimated: Bool = false
+    
+    
+    
+    var body: some View {
+        VStack {
+            Button("button") {
+                withAnimation(
+                    .default
+                    //.repeatForever(autoreverses: true)  povtorenie bez limita
+                    .repeatCount(5, autoreverses: true))
+                {
+                    isAnimated.toggle()
+                }
+                
+            }
+            
+            
+            Spacer()
+            RoundedRectangle(cornerRadius: isAnimated ? 50 : 25)
+                .fill(isAnimated ? Color.red : Color.green)
+                .frame(width: isAnimated ? 100 : 300,
+                       height: isAnimated ? 100 : 300)
+                .rotationEffect(Angle(degrees: isAnimated ? 360 : 0 ))
+                .offset(y: isAnimated ? 300 : 0) // smeshenie
+                .animation(.default
+                            .repeatForever(autoreverses: true)) // mozhno i tak animirovat bez togo chto v knopke no chashe i lyche isp kak v knopke
+            
+            Spacer()
+        }
+    }
+    
+    
+}
+
+
+
+
+
+struct AddingAnimations_Previews: PreviewProvider {
+    static var previews: some View {
+        AddingAnimations()
+    }
+}
