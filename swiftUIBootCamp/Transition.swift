f315ecf51216575bac14c5d0e88ae95ec92aa525
Adham <adam.bolotov@gmail.com>
2022-02-23 05:56:48 +0100

    end transition

---
+++ swiftUIBootCamp/Transition.swift
@@ -0,0 +1,62 @@
+
+import SwiftUI
+
+
+struct Transition: View {
+    
+    @State var showView: Bool = false
+    
+    
+    var body: some View {
+       
+        
+        // v kratce trnsition nyzhen kogda hochesh ispolzovat poyzvlyaushiesya obecty a animacci dlya yzhe imeushihsya obektov na ekrane
+        
+        
+        ZStack (alignment: .bottom) {
+            
+            
+            
+            
+            VStack {
+                Button("button") {
+                    showView.toggle()                }
+                Spacer()
+            }
+            
+            if showView {
+            RoundedRectangle(cornerRadius: 30)
+                .frame(height: UIScreen.main.bounds.height * 0.5)
+                .transition(.asymmetric(
+                    insertion: .move(edge: .bottom),
+                    removal: AnyTransition.opacity.animation(.easeInOut)
+                ))
+                .animation(.easeInOut)
+                
+              //.transition(AnyTransition.scale.animation(.easeInOut))
+              //.transition(AnyTransition.opacity.animation(.easeInOut))
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        
+        
+    }
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+struct Transition_Previews: PreviewProvider {
+    static var previews: some View {
+        Transition()
+    }
+}
