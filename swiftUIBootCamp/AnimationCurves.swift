5e50ae8f29a8e2c12dce33944ee68a70bc1642aa
Adham <adam.bolotov@gmail.com>
2022-02-23 04:10:19 +0100

    end animal curvs

--- swiftUIBootCamp/AnimationCurves.swift
+++ swiftUIBootCamp/AnimationCurves.swift
@@ -22,21 +22,27 @@ struct AnimationCurves: View {
             
             RoundedRectangle(cornerRadius: 20)
                 .frame(width: isAnimating ? 350 : 50, height: 100)
-                .animation(.linear(duration: timing), value: isAnimating)
+                .animation(.spring(
+                    response: 0.5,
+                    dampingFraction: 0.7,
+                    blendDuration: 1),
+                           value: isAnimating)
+                //.animation(.spring(),  value: isAnimating)
+                //.animation(.linear(duration: timing), value: isAnimating)
             
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: isAnimating ? 350 : 50, height: 100)
-                .animation(.easeIn(duration: timing), value: isAnimating)
-            
-            
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: isAnimating ? 350 : 50, height: 100)
-                .animation(.easeInOut(duration: timing), value: isAnimating)
-            
-            
-            RoundedRectangle(cornerRadius: 20)
-                .frame(width: isAnimating ? 350 : 50, height: 100)
-                .animation(.easeOut(duration: timing), value: isAnimating)
+//            RoundedRectangle(cornerRadius: 20)
+//                .frame(width: isAnimating ? 350 : 50, height: 100)
+//                .animation(.easeIn(duration: timing), value: isAnimating)
+//
+//
+//            RoundedRectangle(cornerRadius: 20)
+//                .frame(width: isAnimating ? 350 : 50, height: 100)
+//                .animation(.easeInOut(duration: timing), value: isAnimating)
+//
+//
+//            RoundedRectangle(cornerRadius: 20)
+//                .frame(width: isAnimating ? 350 : 50, height: 100)
+//                .animation(.easeOut(duration: timing), value: isAnimating)
 
         }
         
