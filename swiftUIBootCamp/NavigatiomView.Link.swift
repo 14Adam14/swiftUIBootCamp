6bb9011d4ae16029deec7110b873e8ff0eea28f6
Adham <adam.bolotov@gmail.com>
2022-02-26 15:24:01 +0100

    start navigation

---
+++ swiftUIBootCamp/NavigatiomView.Link.swift
@@ -0,0 +1,22 @@
+
+import SwiftUI
+
+struct NavigatiomView_Link: View {
+    var body: some View {
+        NavigationView{
+            Text("Hello, World!")
+        }
+        
+    }
+}
+
+
+
+
+
+
+struct NavigatiomView_Link_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigatiomView_Link()
+    }
+}
