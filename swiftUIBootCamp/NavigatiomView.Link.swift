663f33becd7df172913ba17ee3713a8deb600dcc
Adham <adam.bolotov@gmail.com>
2022-02-27 14:28:21 +0100

    proces

--- swiftUIBootCamp/NavigatiomView.Link.swift
+++ swiftUIBootCamp/NavigatiomView.Link.swift
@@ -3,14 +3,37 @@ import SwiftUI
 
 struct NavigatiomView_Link: View {
     var body: some View {
-        NavigationView{
-            Text("Hello, World!")
+        NavigationView {
+            ScrollView {
+                
+                NavigationLink("hola world") {
+                    MyOtherScreen()    //destination
+                }
+                
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
+            .navigationTitle("all boxe")
+//            .navigationBarTitleDisplayMode(.automatic)
+//            .navigationBarHidden(true)
         }
         
     }
 }
 
-
+struct MyOtherScreen: View {
+    var body: some View {
+        ZStack {
+            Color.green.ignoresSafeArea()
+                .navigationTitle("veryle")
+            
+            NavigationLink("click the") {
+                Text("leayi ne")
+            }
+        }
+    }
+}
 
 
 
