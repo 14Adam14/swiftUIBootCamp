cbccdff6d14c70a16d911be7a6d9ce65906098c7
Adham <adam.bolotov@gmail.com>
2022-03-03 17:21:07 +0100

    nd tabview

--- swiftUIBootCamp/TabView.PageTabView.swift
+++ swiftUIBootCamp/TabView.PageTabView.swift
@@ -1,37 +1,58 @@
 
 import SwiftUI
 
+
+// не забывать добавлять  .tag(0)   номер
+
 struct TabView_PageTabView: View {
     
     
     @State var selectTab: Int = 0
     
+    let icons: [String] = [
+    "heart.fill", "globe", "house.fill", "person.fill"
+    ]
+    
     
     var body: some View {
         
-        TabView(selection: $selectTab ) {
-            
-            HomeView(selectTab: $selectTab)
-                .tabItem {
-                    Image(systemName: "house.fill")
-                    Text("home")
-                }
-                .tag(0)
-            Text("browse tab")
-                .tabItem {
-                    Image(systemName: "globe")
-                    Text("browse")
-                }
-                .tag(1)
-            Text("people tab")
-                .tabItem {
-                    Image(systemName: "person.fill")
-                    Text("profile")
-                }
-                .tag(2)
+        TabView {
+            ForEach(icons, id: \.self) {icon in
+                Image(systemName: icon)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(30)
+            }
         }
-        .accentColor(.pink
-        )
+        .background(
+            RadialGradient(gradient: Gradient(colors: [Color.red, Color.blue]), center: .center, startRadius: 5, endRadius: 300))
+        .frame(height: 300)
+        .cornerRadius(15)
+        .tabViewStyle(PageTabViewStyle())
+        //
+        //        TabView(selection: $selectTab ) {
+        //
+        //            HomeView(selectTab: $selectTab)
+        //                .tabItem {
+        //                    Image(systemName: "house.fill")
+        //                    Text("home")
+        //                }
+        //                .tag(0)
+        //            Text("browse tab")
+        //                .tabItem {
+        //                    Image(systemName: "globe")
+        //                    Text("browse")
+        //                }
+        //                .tag(1)
+        //            Text("people tab")
+        //                .tabItem {
+        //                    Image(systemName: "person.fill")
+        //                    Text("profile")
+        //                }
+        //                .tag(2)
+        //        }
+        //        .accentColor(.pink
+        //        )
         
     }
 }
@@ -52,7 +73,7 @@ struct HomeView: View {
             VStack {
                 Text("home tab")
                     .font(.largeTitle)
-                .foregroundColor(.white)
+                    .foregroundColor(.white)
                 
                 
                 Button {
@@ -65,12 +86,12 @@ struct HomeView: View {
                         .background(Color.white)
                         .cornerRadius(10)
                 }
-
+                
             }
             
             
-          
+            
         }
-
+        
     }
 }
