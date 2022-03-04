4d50c0c90cdcab59c5f210d1e9c0976fb19d47fe
Adham <adam.bolotov@gmail.com>
2022-03-04 12:17:40 +0100

    nd

--- swiftUIBootCamp/onAppear.onDisappear.swift
+++ swiftUIBootCamp/onAppear.onDisappear.swift
@@ -1,11 +1,44 @@
 
 import SwiftUI
 
+
+
 struct onAppear_onDisappear: View {
+    
+    @State var myText: String = "start text"
+    @State var count: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                Text(myText)
+                LazyVStack {
+                    ForEach(0..<50) {_ in
+                        RoundedRectangle(cornerRadius: 25)
+                            .frame(height: 200)
+                            .padding()
+                            .onAppear {
+                            count += 1
+                        }
+                    }
+                }
+            }
+            .onAppear(perform: {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    myText = "new text"
+                }
+                
+            })
+            .onDisappear(perform: {
+                myText = "end text"
+            })
+            .navigationTitle("On Appear: \(count)")
+        }
     }
 }
+
+
+
 
 struct onAppear_onDisappear_Previews: PreviewProvider {
     static var previews: some View {
